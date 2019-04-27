9b3455017b3885bf265b5bf4f1a71456d75baa4b
mark long <mark@locatechs.com>
2019-04-27 20:43:52 +0200

    django running

--- django_project/web/settings.py
+++ django_project/web/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo',
+        'USER': 'demo',
+        'PASSWORD': 'demo',
+        'HOST': 'db',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
